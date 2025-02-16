7c9bd3ecc1edeb25405ed0d45c0f20517fe7c9ff
anand <prkshanand084@gmail.com>
2025-02-17 00:01:35 +0530

    modified and create

---
+++ day-4-s3-dynamodb-resource-creation/main.tf
@@ -0,0 +1,23 @@
+
+#creating s3 bucket and dynamo DB for state backend storgae and applying the lock mechanisam for statefile
+
+provider "aws" { 
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "prakash-terraform-state-bucket"
+  
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- day-4-state-remote-backend/backend.tf
+++ day-4-state-remote-backend/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "prakash-terraform-state-bucket"
     key           = "terraform.tfstate"
     region        = "us-east-1"
-    dynamodb_table = ""
+    dynamodb_table = "terraform-state-lock-dynamo"
+    encrypt = true
   }
 }
 
