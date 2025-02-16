f017671415e389402972cceb4c3ff537b951c13e
anand <prkshanand084@gmail.com>
2025-02-16 14:40:49 +0530

    day4-remote

---
+++ day-4-state-remote-backend/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "prakash-terraform-state-bucket"
+    key           = "terraform.tfstate"
+    region        = "us-east-1"
+    dynamodb_table = ""
+  }
+}
+
---
+++ day-4-state-remote-backend/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "name" {
+ami = "ami-085ad6ae776d8f09c"
+instance_type = "t2.micro"
+key_name = "mynewkeypair"
+availability_zone = "us-east-1b"
+
+tags = {
+  Name = "dev"
+}
+
+}
