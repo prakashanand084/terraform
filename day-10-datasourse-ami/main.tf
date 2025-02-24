1b012c87fddd85942658faa225d5198d12fcda41
PRAKASH ANAND <134529932+prakashanand084@users.noreply.github.com>
2025-02-24 15:55:49 +0530

    Update main.tf

--- day-10-datasourse-ami/main.tf
+++ day-10-datasourse-ami/main.tf
@@ -1,3 +1,32 @@
+provider "aws" {
+  
+}
+#calling Default AMI process as a Data source
+
+# data "aws_ami" "amzlinux" {
+#   most_recent = true
+#   owners = [ "amazon" ]
+#   filter {
+#     name = "name"
+#     values = [ "amzn2-ami-hvm-*-gp2" ]
+#   }
+#   filter {
+#     name = "root-device-type"
+#     values = [ "ebs" ]
+#   }
+#   filter {
+#     name = "virtualization-type"
+#     values = [ "hvm" ]
+#   }
+#   filter {
+#     name = "architecture"
+#     values = [ "x86_64" ]
+#   }
+# }
+
+
+#Calling custom AMI process as data source
+
 data "aws_ami" "amzlinux" {
   most_recent = true
   owners = [ "self" ]
@@ -10,4 +39,7 @@ data "aws_ami" "amzlinux" {
 resource "aws_instance" "dev-anand" {
   ami = data.aws_ami.amzlinux.id
   instance_type = "t2.micro"
+tags = {
+      Name = "dev"
+    }
 }
