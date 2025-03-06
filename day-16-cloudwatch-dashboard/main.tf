a44a787ab88582f11a118f4c087a27b32d264ed9
anand <prkshanand084@gmail.com>
2025-03-07 01:52:18 +0530

    day-16

---
+++ day-16-cloudwatch-dashboard/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_cloudwatch_dashboard" "example_dashboard" {
+  dashboard_name = "MyDashboard"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 6
+        height = 6
+        properties = {
+          metrics = [
+            ["AWS/EC2", "CPUUtilization", "InstanceId", "i-0876278195b5e6d30"]
+          ]
+          period     = 300
+          stat       = "Average"
+          region     = "us-east-1"
+          title      = "EC2 CPU Utilization"
+        }
+      },
+      {
+        type   = "metric"
+        x      = 6
+        y      = 0
+        width  = 6
+        height = 6
+        properties = {
+          metrics = [
+            ["AWS/Lambda", "Invocations", "FunctionName", "my-lambda-function"]
+          ]
+          period     = 60
+          stat       = "Sum"
+          region     = "us-east-1"
+          title      = "Lambda Invocations"
+        }
+      }
+    ]
+  })
+}
